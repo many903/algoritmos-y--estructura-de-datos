f328fcba841dc7daa5b8ec531b1c78034eb74b89
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2022-11-19 21:24:49 -0600

    Create tarea3.c

---
+++ tarea3.c
@@ -0,0 +1,54 @@
+Class Nodo
+{
+    T dato;
+    Nodo* next;
+
+public:
+    T getDato(){
+        return this->dato;
+    }
+    void setDato(T dato){
+    this->dato = dato;
+    }
+    Nodo* getNext(){
+        return this->next;
+    }
+    void setNext(Nodo* next){
+        this->next = next;
+    }
+    Nodo(T dato, Nodo* next){
+        this->dato = dato;
+        this->next = next;
+    }
+};
+template  typename T
+class LinkedList{
+    |                    ^
+    int size;
+    Nodo<T>* head;
+
+public:
+    int getSize(){
+        return this->size;
+    }
+    void setSize(int size){
+        this->size = size;
+    }
+    Nodo<T>* getHead(){
+        return this->head;
+    }
+    void setHead(Nodo<T>* head){
+        this->head = head;
+    }
+    LinkedList(){
+        this->head = nullptr;
+        this->size = 0;
+    }
+    void agregarInicio(T valor){
+        Nodo<T>* nuevo = new Nodo<T>(valor,this->head);
+        this->head = nuevo;
+        this->size++;
+    }
+    void agregarFinal(T valor){
+        Nodo<T>* nuevo = new Nodo<T>(valor,nullptr);
+       

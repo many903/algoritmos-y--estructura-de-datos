e734d9679d5bfce32c90f132b101da4c61a26498
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2022-11-20 19:34:18 -0600

    3.1

--- tarea3.c
+++ tarea3.c
@@ -1,8 +1,32 @@
-
+/*tarea 3  ejrcicio 1*/
+#include <stdio.h>
+#include <stdlib.h>
  
-
 typedef struct molde
 {
     int dato;
     struct Molde *siguiente;
 } Nodo;
+
+/*varible global "inicio de lista" */
+nodo *inicio = NULL;
+
+/*incertar el valor al inicio de la lista */
+void insertar(int valor)
+{   nodo*nuevo;
+    nuevo =  malloc(sizeof(nodo));
+    nuevo -> dato = valor;
+    if(valor == NULL) {
+        inicio = nuevo;
+        nuevo -> siguiente = NULL;
+    }
+    else{
+        nuevo -> siguiente = inicio;
+        inicio = nuevo;
+    }
+}
+int main()
+{
+    insertar
+    printf("Dato de en inicio: %d\n",inicio->dato);
+}

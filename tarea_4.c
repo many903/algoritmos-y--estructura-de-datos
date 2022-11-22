a4e56da475577782bcf7afbc9c5137fe8f6371aa
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2022-11-22 02:51:54 +0000

    tarea 4 prueva º1

--- tarea_4.c
+++ tarea_4.c
@@ -4,3 +4,40 @@ ejecutar las 5 operaciones básicas descritas en la unidad 3. Luego, desde un m�
 instrucciones soportadas. Para la operación 2, puedes tomar el mismo código del ciclo
 de recorrido que propusimos en la Tarea 3. 
 Manuel Enrique Garcia Neve 2193077979*/
+
+
+//no se si estoy bien aun me falta 
+
+#include <stdio..h>
+#include<stdlib.h>
+
+typedef struct Molde 
+{   int dato;
+    struct Molde *siguiente;
+}   Nodo;
+
+Nodo *inicio = NULL;
+
+void insertar ( int x)
+{
+    Nodo *nuevo;
+    nuevo = malloc(sizeof(Nodo));
+    nuevo-> dato = x;
+    if (inicio == NULL)
+    {   inicio = nuevo;
+        nuevo -> siguiente =NULL;
+    }
+    else 
+    { nuevo ->siguiente = inicio;
+      inicio = nuevo;
+    }  
+}
+
+int main()
+{
+    insertar(4);
+    printf("Dato en inicio : %d\n" ,inicio->dato) ;
+    
+    return 0;
+}   
+    

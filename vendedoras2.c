787055c77f2d02070886896467ee27a2bcd14efc
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2022-12-27 21:19:15 -0600

    hooa

---
+++ vendedoras2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Estructura para almacenar el nombre y las ventas de cada vendedor
+typedef struct {
+    char nombre[20];
+    int ventas;
+} Vendedor[n];
+
+// Función que compara dos vendedores según sus ventas
+int comparacion (){
+
+}
+
+int lee_vendedores(){
+    
+}
+int main() {
+    /*lee cuantos son (n) y cuantos tengo que dar(k)*/
+    int n, k;
+    n=k=0;
+    scanf("%d %d", &n, &k);
+    /*lee vendedores*/
+    if n>0{
+        lee_vendedores();
+    }
+    return 0;
+}

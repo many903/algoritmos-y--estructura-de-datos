41cd6c5ffff9901af1e444d7b81268b8cc4e31c8
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2023-01-01 18:15:29 -0600

    fibonacci

--- fibonacci.c
+++ fibonacci.c
@@ -25,11 +25,29 @@ void insertar(int x)
         inicio = nuevo;
     }
 }
+int fibonacci1(n){
+    numIter++;
+    if (n<2){
+        //Caso trivial
+        return n;
+    } else {
+        //Descomponemos problema n en 2 subproblemas n-1 y n-2
+        let [p1, p2] = [n-1, n-2];
+        //1 llamada por cada subproblema
+        let s1 = fibonacci1(p1);
+        let s2 = fibonacci1(p2);
+        //Combinamos soluciones parciales
+        result = s1+s2
+        return result;
+    }
+}
 
 int main ()
 {
     int n = scanf("cuantos juegan :%d", &n)
     insertar(n);
     printf("Dato de inicio(lls): %d\n",inicio->dato);
+    fibonacci(n);
+    printf ("dato de fibonacci:%d\n",fibonacci->result);
     return 0;
 }
